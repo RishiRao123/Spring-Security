a7e5621eb9126380d9f03f49be9e4154ba894a07
RishiRao123 <rishiraopuskuri@gmail.com>
2026-03-10 19:05:01 +0530

    Entity added

---
+++ springSecurity2/src/main/java/org/raoamigos/springsecurity2/entity/Users.java
@@ -0,0 +1,25 @@
+package org.raoamigos.springsecurity2.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role;
+}
